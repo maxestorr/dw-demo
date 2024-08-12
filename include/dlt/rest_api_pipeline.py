a456aef571019494b6e5e49b6915c792c67b42a9
maxestorr <maxestorr@gmail.com>
2024-08-12 23:25:35 +0100

    working ingestion from ebird api with dlt

--- include/dlt/rest_api_pipeline.py
+++ include/dlt/rest_api_pipeline.py
@@ -1,18 +1,22 @@
+import logging
 from typing import Any
 
 import dlt
 from rest_api import (
     RESTAPIConfig,
-    check_connection,
-    rest_api_source,
     rest_api_resources,
 )
+
+
+log = logging.getLogger(__name__)
+log.setLevel(level=logging.DEBUG)
+log.debug("Starting rest_api_pipeline.py")
+
 
 @dlt.source
 def github_source(github_token: str = dlt.secrets.value) -> Any:
     # Create a REST API configuration for the GitHub API
     # Use RESTAPIConfig to get autocompletion and type checking
-
     config: RESTAPIConfig = {
         "client": {
             "base_url": "https://api.github.com/repos/dlt-hub/dlt/",
@@ -105,65 +109,51 @@ def load_github() -> None:
 
 
 @dlt.source
-def pokemon_source() -> Any:
+def ebird_source(ebird_token: str=dlt.secrets.value) -> Any:
+    log.debug(f"In ebird_source()")
     config: RESTAPIConfig = {
         "client": {
-            "base_url": "https://pokeapi.co/api/v2/",
+            "base_url": "https://api.ebird.org/v2/",
+            "auth": {
+                "type": "api_key",
+                "name": "x-ebirdapitoken",
+                "api_key": f"{ebird_token}",
+                "location": "header",
+            },
         },
         "resource_defaults": {
             "write_disposition": "replace",
-            "endpoint": {
-                "params":{
-                    "limit": 1000,
-                }
-            }
         },
         "resources": [
-            "pokemon",
-            "berry",
-            "location",
+            {
+                "name": "top100",
+                "endpoint": {
+                    "path": "product/top100/{region_code}/{year}/{month}/{day}",
+                    "params": {
+                        "region_code": "CA",
+                        "year": "2024",
+                        "month": "08",
+                        "day": "01",
+                    },
+                },
+            },
         ],
     }
 
     yield from rest_api_resources(config)
 
 
-def load_pokemon() -> None:
+def load_ebird() -> None:
+    log.debug("In load_ebird()")
     pipeline = dlt.pipeline(
-        pipeline_name="rest_api_pokemon",
+        pipeline_name="rest_api_ebird",
         destination='duckdb',
-        dataset_name="rest_api_data",
+        dataset_name="ebird",
     )
 
-    load_info = pipeline.run(pokemon_source())
+    load_info = pipeline.run(ebird_source())
     print(load_info)
 
 
-
-@dlt.source
-def ebird_source(ebird_token: str=dlt.secrets.value) -> Any:
-    config: RESTAPIConfig = {
-        "client": {
-            "base_url": "https://pokeapi.co/api/v2/",
-        },
-        "resource_defaults": {
-            "write_disposition": "replace",
-            "endpoint": {
-                "params":{
-                    "limit": 1000,
-                }
-            }
-        },
-        "resources": [
-            "pokemon",
-            "berry",
-            "location",
-        ],
-    }
-
-    yield rest_api_resources(config)
-
-
 if __name__ == "__main__":
-    # load_github()
-    load_pokemon()
+    load_ebird()
